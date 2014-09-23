7b22ebd2f1c6d02a14e4e11326995d806116895d
Vince Via <vincent.via@theice.com>
2014-09-23 09:06:17 -0400

    oa

--- src/main/java/com/viaoa/jfc/table/OATableColumn.java
+++ src/main/java/com/viaoa/jfc/table/OATableColumn.java
@@ -111,6 +111,7 @@ public class OATableColumn {
 
     private Hub hubMethodHub; // 2006/12/11
 
+/***qqqqqqq OLD    
     // methods gets set to null whenever Hub or PropertyPath get changed
     public Method[] getMethods_OLD(Hub hub) {
         if (methods != null && hub == hubMethodHub) return methods;
@@ -234,6 +235,7 @@ public class OATableColumn {
 
         return methods;
     }
+*/
 
     public void setMethods(Method[] m) {
         methods = m;
