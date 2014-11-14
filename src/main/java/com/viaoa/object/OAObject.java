51cac0a16101773811662f7edc920e10df139551
vvia <vvia@viaoa>
2014-11-13 21:40:33 -0500

    new version number

--- src/main/java/com/viaoa/object/OAObject.java
+++ src/main/java/com/viaoa/object/OAObject.java
@@ -106,7 +106,7 @@ public class OAObject implements java.io.Serializable, Comparable {
     private static final long serialVersionUID = 1L; // internally used by Java Serialization to identify this version of OAObject.
 
     
-    public static final int version = 1411050;  
+    public static final int version = 1411140;  
     static {
         Properties props = System.getProperties();
         
