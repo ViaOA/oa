2a76ba65178baf525c0777ed5ab7f737fab70d43
vvia <vvia@viaoa>
2015-08-22 15:51:28 -0400

    xml test

---
+++ src/test/java/com/viaoa/util/OAXMLlReaderTest.java
@@ -0,0 +1,44 @@
+package com.viaoa.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.viaoa.OAUnitTest;
+import com.viaoa.object.OAObject;
+
+public class OAXMLlReaderTest extends OAUnitTest {
+
+    
+    @Test
+    public void test() throws Exception {
+        
+        // simple test to load into oaobjects 
+        
+        String s;
+        s = "<xml>"; // must wrap in outer tag
+        s += "<ssh><command>runcommand</command><output>output text here</output></ssh>";
+        s += "<ssh><command>runcommand2</command><output>output text here2</output></ssh>";
+        s += "</xml>";
+        
+        OAXMLReader xr = new OAXMLReader() {
+            @Override
+            protected String resolveClassName(String className) {
+                return null;
+            }
+        };
+        Object objx = xr.parseString(s);
+        Object[] objs = xr.getRootObjects();
+        
+        assertEquals(2, objs.length);
+        
+        assertEquals(OAObject.class, objs[0].getClass());
+        
+        OAObject oaobj = (OAObject) objs[1];
+        Object val = oaobj.getProperty("command");
+        assertEquals("runcommand2", val);
+        
+    }
+
+
+}
