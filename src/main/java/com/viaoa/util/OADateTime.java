ed33351ad8b9fc65ccff86735fa587617fd88d75
Vince Via <vincent.via@theice.com>
2015-11-06 14:54:31 -0500

    table sort arrows flipped

--- src/main/java/com/viaoa/util/OADateTime.java
+++ src/main/java/com/viaoa/util/OADateTime.java
@@ -48,7 +48,7 @@ import java.sql.Time;
     '' single quote            (Literal)           '
 
     Examples:
-    "yyyy.MM.dd G 'at' hh:mm:ss z"    ->>  1996.07.10 AD at 15:08:56 PDT
+    "yyyy.MM.dd G 'at' HH:mm:ss z"    ->>  1996.07.10 AD at 15:08:56 PDT
     "EEE, MMM d, ''yy"                ->>  Wed, July 10, '96
     "h:mm a"                          ->>  12:08 PM
     "hh 'o''clock' a, zzzz"           ->>  12 o'clock PM, Pacific Daylight Time
