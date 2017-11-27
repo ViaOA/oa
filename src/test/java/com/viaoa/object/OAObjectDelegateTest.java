e580deecabe1093cc5fe2c6370fcc9cd4c83fd01
vvia <vvia@viaoa>
2017-11-27 13:50:39 -0500

    unit test

--- src/test/java/com/viaoa/object/OAObjectDelegateTest.java
+++ src/test/java/com/viaoa/object/OAObjectDelegateTest.java
@@ -5,11 +5,11 @@ import static org.junit.Assert.*;
 
 import com.viaoa.OAUnitTest;
 
-import test.theice.tsac3.model.oa.*;
+import test.theice.tsac2.model.oa.*;
 
 public class OAObjectDelegateTest extends OAUnitTest {
 
-    @Test
+//    @Test
     public void getAutoAddTest() {
         assertFalse(OAObjectDelegate.getAutoAdd(null));
         
@@ -21,9 +21,64 @@ public class OAObjectDelegateTest extends OAUnitTest {
         
         server.setAutoAdd(true);
         assertTrue(OAObjectDelegate.getAutoAdd(server));
-        
     }
     
-    
-    
+    @Test
+    public void getAutoAddTest2() {
+        Silo silo = new Silo();
+        assertTrue(OAObjectDelegate.getAutoAdd(silo));
+        assertTrue(silo.getAutoAdd());
+        
+        Server server = new Server();
+        assertTrue(OAObjectDelegate.getAutoAdd(server));
+        assertTrue(server.getAutoAdd());
+        
+        server.setAutoAdd(false);
+        assertFalse(server.getAutoAdd());
+
+        server.setSilo(silo);
+        assertFalse(server.getAutoAdd());
+
+        assertEquals(server.getSilo(), silo);
+        assertEquals(0, silo.getServers().size());
+        
+        server.setAutoAdd(true);
+        assertEquals(server.getSilo(), silo);
+        assertEquals(1, silo.getServers().size());
+        assertEquals(silo.getServers().getAt(0), server);
+        assertTrue(server.getAutoAdd());
+        
+        server = new Server();
+        server.setAutoAdd(false);
+        assertFalse(server.getAutoAdd());
+        
+        silo.getServers().add(server);
+        assertFalse(server.getAutoAdd());
+        
+        assertEquals(server.getSilo(), silo);
+        assertEquals(2, silo.getServers().size());
+        
+        Application app = new Application();
+        server.getApplications().add(app);
+        assertFalse(app.getAutoAdd());
+        
+        server.save();
+        assertTrue(server.getAutoAdd());
+        assertTrue(app.getAutoAdd());
+        
+        app = new Application();
+        app.setServer(server);
+        assertTrue(app.getAutoAdd());
+        assertEquals(2, server.getApplications().getSize());
+        
+        app = new Application();
+        app.setAutoAdd(false);
+        app.setServer(server);
+        assertFalse(app.getAutoAdd());
+        assertEquals(2, server.getApplications().getSize());
+
+        app.save();
+        app.setAutoAdd(true);
+        assertEquals(3, server.getApplications().getSize());
+    }
 }
