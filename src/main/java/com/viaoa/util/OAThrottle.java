2280063d2b51f364ab66fb055c05e82386a60e22
vvia <vvia@DESKTOP-FDF0RC3>
2018-01-07 14:03:24 -0500

    new class used for throttling

---
+++ src/main/java/com/viaoa/util/OAThrottle.java
@@ -0,0 +1,64 @@
+package com.viaoa.util;
+
+import java.util.concurrent.atomic.*;
+
+/**
+ * Used to throttle based on a minimum time (ms) frame.
+ * Has a check() method that can be called to know if it's time for further action.
+ * @author vvia
+ */
+public class OAThrottle {
+
+    private final AtomicLong aiMsLast = new AtomicLong();
+    private final AtomicLong aiCnt = new AtomicLong();
+    private long msDelay;
+
+  
+    public OAThrottle(long msDelay) {
+        setDelay(msDelay);
+    }
+
+    public void setDelay(long msDelay) {
+        this.msDelay = msDelay;
+    }
+    public long getDelay() {
+        return msDelay;
+    }
+    
+    /**
+     * This will check to see if the the required delay/time has passed since the last call to check.
+     * @return
+     */
+    public boolean check() {
+        aiCnt.incrementAndGet();
+        long msNow = System.currentTimeMillis();
+        if (aiMsLast.get() + msDelay > msNow) {
+            return false;
+        }
+        aiMsLast.set(msNow);
+        return true;
+    }
+    
+    public long now() {
+        long ms = System.currentTimeMillis();
+        return ms;
+    }
+
+    /**
+     * sets throttle counter to and last valid check time to 0L.
+     */
+    public void reset() {
+        aiMsLast.set(0);
+        aiCnt.set(0);
+    }
+    
+    public long getCheckCount() {
+        return aiCnt.get();
+    }
+    /**
+     * Returns the last time that a call to check() returned true.
+     */
+    public long getLastThrottle() {
+        return aiMsLast.get();
+    }
+}
