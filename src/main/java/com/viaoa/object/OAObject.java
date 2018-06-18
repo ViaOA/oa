f611c3517f2171a3f4c91e3912c3c0cdd5dbac9a
vvia <vvia@DESKTOP-FDF0RC3>
2018-06-18 08:32:44 -0400

    version 3.5.55

--- src/main/java/com/viaoa/object/OAObject.java
+++ src/main/java/com/viaoa/object/OAObject.java
@@ -108,7 +108,7 @@ public class OAObject implements java.io.Serializable, Comparable {
     }
     
     static {
-        String ver = "3.5.54_20180614";
+        String ver = "3.5.55_20180618";
         try {
             InputStream resourceAsStream = OAObject.class.getResourceAsStream("/META-INF/maven/com.viaoa/oa/pom.properties");
             Properties props = new Properties();
