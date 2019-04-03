7c7d57f8488e4c0b459aff8b2c50240bca2b3dfb
vvia <vvia@DESKTOP-FDF0RC3.mshome.net>
2019-04-03 13:55:05 -0400

    javadoc

--- src/main/java/com/viaoa/context/package-info.java
+++ src/main/java/com/viaoa/context/package-info.java
@@ -1,7 +1,8 @@
 /**
+ * Defines an application context, used for managing a user or processes permissions.
+ * <p>
+ * Automates how applications and object graphs work with permissions.  
  * 
- */
-/**
  * @author vvia
  *
  */
